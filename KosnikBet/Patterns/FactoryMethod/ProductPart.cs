9370cc01b16fdf451c8baeec05c3a7f4c6907d48
Mouse Mickey <mickeymouse.kz.ru@outlook.com>
2022-09-19 08:29:30 +0300

    Created FactoryMethod

---
+++ KosnikBet/Patterns/FactoryMethod/ProductPart.cs
@@ -0,0 +1,22 @@
+﻿namespace KosnikBet.Patterns.FactoryMethod
+{
+    internal interface IProduct
+    {
+        void DoSomething();
+    }
+
+    internal class FirstProduct: IProduct
+    {
+        public void DoSomething()
+        {
+            
+        }
+    }
+    internal class SecondProduct: IProduct
+    {
+        public void DoSomething()
+        {
+
+        }
+    }
+}
