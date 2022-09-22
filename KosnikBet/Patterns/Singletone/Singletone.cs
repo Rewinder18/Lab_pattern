0d942aefc9af890b8230d52cc332cf27282405ce
Mouse Mickey <mickeymouse.kz.ru@outlook.com>
2022-09-22 07:12:51 +0300

    Created Singleton Pattern

---
+++ KosnikBet/Patterns/Singletone/Singletone.cs
@@ -0,0 +1,22 @@
+﻿namespace KosnikBet.Patterns.Singletone
+{
+    internal sealed class Singletone
+    {
+        private static Singletone _instance;
+
+        private Singletone()
+        {
+
+        }
+
+        public static Singletone GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singletone();
+            }
+
+            return _instance;
+        }
+    }
+}
